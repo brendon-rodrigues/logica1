feff923090cff046c14ed150031a581da1fcac46
Brendon <rodriguesbrendon861@gmail.com>
2022-01-12 20:45:34 -0300

    repositorio criado

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Brendon"
+let numero = 100
+let anoAtual = 2022 - numero
+console.log(anoAtual)
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true
+console.log(quartaFeira)
